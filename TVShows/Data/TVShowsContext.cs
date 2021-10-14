f41d78dec2426d5b9012ebdf77ef2eb2629ea858
Dalton <caseyde@hendrix.edu>
2021-10-13 22:21:31 -0500

    website creation

---
+++ TVShows/Data/TVShowsContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TVShows.Models;
+
+namespace TVShows.Data
+{
+    public class TVShowsContext : DbContext
+    {
+        public TVShowsContext (DbContextOptions<TVShowsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TVShows.Models.Show> Show { get; set; }
+    }
+}
---
+++ TVShows/Pages/Shows/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using TVShows.Data;
+using TVShows.Models;
+
+namespace TVShows.Pages.Shows
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly TVShows.Data.TVShowsContext _context;
+
+        public DetailsModel(TVShows.Data.TVShowsContext context)
+        {
+            _context = context;
+        }
+
+        public Show Show { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Show = await _context.Show.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Show == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
